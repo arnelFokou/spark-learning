41f1a9b94546273d2e88b8b66847a9001c1aecd7
arnel <arnelfokou6@gmail.com>
2025-12-27 10:58:57 +0100

    feature:first spark project

---
+++ spark_scripts/first_project.py
@@ -0,0 +1,23 @@
+from pyspark import SparkContext, SparkConf
+
+def get_rdd_kv(row):
+    row_splitted = row.split(",")
+    user_id = row_splitted[0]
+    amount = float(row_splitted[2])
+    return (user_id,amount)
+
+
+
+conf = SparkConf().setMaster("local").setAppName("amount_spent_by_customer")
+sc = SparkContext(conf=conf)
+
+input = sc.textFile("C:/Users/user/Desktop/projets/spark-learning/spark_files/customer-orders.csv")
+
+input_kv = input.map(get_rdd_kv).reduceByKey(lambda x,y: x+y)
+
+sorted_kv = input_kv.map(lambda x : (x[1],x[0]) ).sortByKey(ascending=False)
+
+results = sorted_kv.collect()
+
+for user,spent_amount in results:
+    print(f"{user} spent {spent_amount:.2f}$")
